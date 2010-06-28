3cac6545bb37a67f95111ac7f9317098fd3629bb
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2010-06-28 15:20:20 +0300

    text changes and small cade cleanup

--- app/controllers/barcodes_controller.rb
+++ app/controllers/barcodes_controller.rb
@@ -14,7 +14,7 @@ class BarcodesController < ApplicationController
   def create_link
     @link_code = LinkCode.new(params[:link_code])
     if @link_code.valid? and @link_code.save
-      render_html(render_to_string(:partial=> 'bar_code', :object=> @link_code))
+      render_response(render_to_string(:partial=> 'bar_code', :object=> @link_code))
     else
       render_error_message(@link_code)
     end
@@ -23,7 +23,7 @@ class BarcodesController < ApplicationController
   def create_sms
     @sms_code = SmsCode.new(params[:sms_code])
     if @sms_code.valid? and @sms_code.save
-      render_html(render_to_string(:partial=> 'bar_code', :object=> @sms_code))
+      render_response(render_to_string(:partial=> 'bar_code', :object=> @sms_code))
     else
       render_error_message(@sms_code)
     end
@@ -32,7 +32,7 @@ class BarcodesController < ApplicationController
   def create_text
     @text_code = TextCode.new(params[:text_code])
     if @text_code.valid? and @text_code.save
-      render_html(render_to_string(:partial=> 'bar_code', :object=> @text_code))
+      render_response(render_to_string(:partial=> 'bar_code', :object=> @text_code))
     else
       render_error_message(@text_code)
     end
@@ -44,7 +44,7 @@ class BarcodesController < ApplicationController
       render :json=> {:error_message=>instance.errors.full_messages.first}.to_json
     end
     
-    def render_html(text)
+    def render_response(text)
       render :json=> {:html=> text}.to_json
     end
 end
