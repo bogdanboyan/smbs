a1f4a361ddafbf096b997909b4db8ed44736ff88
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2010-07-16 20:39:09 +0300

    Added RED ligth for campaign feature

---
+++ features/step_definitions/campaign_steps.rb
@@ -0,0 +1,15 @@
+То /^я должен увидеть список ранее созданых Short адресов$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+То /^когда я выбираю адрес "([^"]*)" из списка$/ do |содержание|
+  pending # express the regexp above with the code you wish you had
+end
+
+То /^я должен увидеть список ранее созданых QR кодов$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+То /^когда я выбираю код "([^"]*)" из списка$/ do |содержание|
+  pending # express the regexp above with the code you wish you had
+end
