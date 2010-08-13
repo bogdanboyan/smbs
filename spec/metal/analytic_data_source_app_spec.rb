29ca7521802d12f6b9b1604c8acd16bbffc945d9
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2010-08-13 21:57:32 +0300

    Added spec for AnalyticDataSourceApp

--- spec/metal/analytic_data_source_app_spec.rb
+++ spec/metal/analytic_data_source_app_spec.rb
@@ -1,15 +1,45 @@
 require File.dirname(__FILE__) + '/../spec_helper'
+require 'rack/mock'
 
 describe AnalyticDataSourceApp do
   
+  DATA_SOURCES = [
+    '/ds/shortener/98/clicks',
+    '/ds/shortener/98/regions'
+  ]
+  
+  UNKNOWN_DATA_SOURCES = [
+    'ds/shortener/98/unknown',
+    'ds/shortener/98',
+    'campaigns/100',
+  ]
+  
   context 'routing context' do
-    
-    it 'should process /ds requests' do
-      ['/ds/shortener/98/clicks'].each do |path|
-        response = AnalyticDataSourceApp.call({'PATH_INFO' => path})
+
+     DATA_SOURCES.each do |path|
+
+      it "should process #{path} request" do
+        req = Rack::Request.new(Rack::MockRequest.env_for("#{path}?tqx=reqId:10"))
+
+        response = AnalyticDataSourceApp.call(req.env)
         response[0].should == 200
       end
     end
     
+     UNKNOWN_DATA_SOURCES.each do |path|
+       
+      it "should not process #{path} request" do
+        req = Rack::Request.new(Rack::MockRequest.env_for("#{path}?tqx=reqId:10"))
+
+        response = AnalyticDataSourceApp.call(req.env)
+        response[0].should == 404
+      end
+    end
+      
   end
+  
+  #TODO: 
+  #specify json table with or without data
+  #GVA error message!!
+
 end
