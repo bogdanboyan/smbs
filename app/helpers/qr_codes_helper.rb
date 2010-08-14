671635990dbb8a473daea28da8cdb5a27fda983a
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2010-08-14 18:42:22 +0300

    Improved QR Codes presentation

---
+++ app/helpers/qr_codes_helper.rb
@@ -0,0 +1,22 @@
+module QrCodesHelper
+  
+  def qr_code_title(qr)
+    desc = case(qr)
+      when LinkCode then "%s" % qr.origin
+      when SmsCode  then "%s (%s)" % [qr.tel, qr.text]
+      when TextCode then "%s" % qr.text
+    end
+    
+    desc.slice(0, 35 - 3) + '...'
+  end
+  
+  def qr_code_description(qr)
+    desc = case(qr)
+      when LinkCode then "ссылка на сайт: <strong>%s</strong>" % qr.origin
+      when SmsCode  then "смс сообщение на номер %s с текстом: %s" % [qr.tel, qr.text]
+      when TextCode then "текст: <strong>%s</strong>" % qr.text.strip
+    end
+    
+    desc.slice(0, 120 - 3) + '...'
+  end
+end
