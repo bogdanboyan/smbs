c5f1da179b780d327d62df90a35efe5ee6d572a5
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2010-08-17 20:19:20 +0300

    Added cukes for download QR image

--- features/step_definitions/qr_codes_steps.rb
+++ features/step_definitions/qr_codes_steps.rb
@@ -21,7 +21,24 @@ end
   has_css?('.history')
   row = список.rows_hash
   within('.history') do
-    qr_code = BarCode.find_by_type(row['type'].camelize)
-    body.should include(qr_code_title(qr_code))
+    @current_qr_code = BarCode.find_by_type(row['type'].camelize)
+    body.should include(qr_code_title(@current_qr_code))
   end
+end
+
+Тогда /^я не должен видеть QR код:$/ do |список|
+  has_css?('.history')
+  row = список.rows_hash
+  within('.history') do
+    @current_qr_code = BarCode.find_by_type(row['type'].camelize)
+    body.should_not include(qr_code_title(@current_qr_code))
+  end
+end
+
+
+Тогда /^я должен загрузить QR код$/ do
+  page.driver.response.status.should == 200
+  page.driver.response.headers['Content-Type'].should              == "image/png"
+  page.driver.response.headers['Content-Transfer-Encoding'].should == "binary"
+  page.driver.response.headers['Content-Disposition'].should       == "attachment; filename=\"#{@current_qr_code.id}.preview.png\""
 end
