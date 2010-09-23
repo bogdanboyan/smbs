570fec7f5ce9746d4df4fb2a2fbb8994415bbb3c
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2010-09-23 21:16:57 +0300

    Added spec for mobile_campaign

--- app/models/mobile_campaign.rb
+++ app/models/mobile_campaign.rb
@@ -10,4 +10,61 @@
 #  updated_at     :datetime
 
 class MobileCampaign < ActiveRecord::Base
+
+  has_many :image_assets
+
+  def document_model_as(format = :json)
+    case format
+      when :json  then attributes['document_model']
+      when :array then ActiveSupport::JSON.decode(attributes['document_model'])
+      else
+        raise 'unknown format: %s' % format
+    end
+  end
+  
+  def validate
+    (errors.add_to_base('document_model is empty') && return) unless attributes['document_model']
+    
+    sanitized_document_model = sanitize(document_model_as(:array))
+    
+    (errors.add_to_base('document_model is empty and sanitized') && return) if (!sanitized_document_model || sanitized_document_model.empty?)
+    
+    sanitized_document_model.each do |partial|
+      type, value = partial['type'], partial['value']
+      case type
+        when 'images'
+          value.each do |image|
+            should_be_true(
+              image.is_a?(Hash) && image.size == 4
+            )
+
+            should_be_true(
+              image['asset_id'].is_a?(String) && image['asset_id'].to_i.is_a?(Numeric),
+              image['style'].is_a?(String),
+              image['width'].is_a?(String),
+              image['path'].is_a?(String)
+            )
+          end
+      end
+    end #each
+  end #def
+
+  def should_be_true(*state)
+    errors.add("value", "has invalid format") unless state.delete_if {|s| s}.empty?
+  end
+
+  def sanitize(document = [])
+    document = document.delete_if do |partial|
+      type, value = partial['type'], partial['value']
+      type.nil? || type =~ /[^header|text|images]/ || value.nil? || value.empty? || sanitize_partial(type, value)
+    end
+    document.compact
+  end
+
+  def sanitize_partial(type, value)
+    case type
+        when 'images' then !value.is_a?(Array) || (value.is_a?(Array) && value.empty?)
+    end
+  end
+  
 end
---
+++ spec/models/mobile_campaign_spec.rb
@@ -0,0 +1,64 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe MobileCampaign do
+  
+  it 'should sanitized' do
+    page = MobileCampaign.new
+    page.sanitize([
+      { 
+        'type'  => 'header',
+        'value' => 'sanizided model'
+      }, {
+        'type'  => 'text',
+        'value' => nil
+      }, {
+        'type'  => 'unknown',
+        'value' => 'abcd'
+      }, {
+        'unknown' => 'type',
+      }, {
+        'type'  => 'images',
+      }, {
+        'type'  => 'images',
+        'value' => 'unknown',
+      }, {
+        'type'  => 'images',
+        'value' => []
+      }, {
+        'type'  => 'header',
+        'value' => ""
+      }
+      
+    ]).should == [{'type' => 'header', 'value' => 'sanizided model'}]
+  end
+
+  context 'with valid document model' do
+    [
+      {
+          :title => 'Untitled 2010-08-28',
+          :document_model => '[{"type":"images","value":[{"width":"101px","asset_id":"7","style":"view","path":"/asset/image/7/view/1282996485.3278.jpg"},{"width":"101px","asset_id":"8","style":"view","path":"/asset/image/8/view/1282996546.43506.jpg"},{"width":"101px","asset_id":"9","style":"view","path":"/asset/image/9/view/1282996564.32426.jpg"}]},{"type":"text","value":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},{"type":"header","value":"Updated content"},{"type":"images","value":[{"width":"194px","asset_id":"8","style":"view","path":"/asset/image/8/view/1282996546.43506.jpg"},{"width":"194px","asset_id":"7","style":"view","path":"/asset/image/7/view/1282996485.3278.jpg"},{"width":"194px","asset_id":"9","style":"view","path":"/asset/image/9/view/1282996564.32426.jpg"}]}]'
+      }
+    ].each do |attributes|
+      it "should validate and create a new instance for: #{attributes[:title]}" do
+        MobileCampaign.create!(attributes)
+      end
+    end
+  end
+  
+  context 'with invalid document model' do
+    [
+      {
+        :title => 'Valid mobile campaign',
+        :document_model => '[{"type":"images","value":[{"asset_id":"7","style":"view","path":"/asset/image/7/view/1282996485.3278.jpg"},{"width":"101px","asset_id":"8","style":"view","path":"/asset/image/8/view/1282996546.43506.jpg"},{"width":"101px","asset_id":"9","style":"view","path":"/asset/image/9/view/1282996564.32426.jpg"}]},{"type":"text","value":"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},{"type":"header","value":"Updated content"},{"type":"images","value":[{"width":"194px","asset_id":"8","style":"view","path":"/asset/image/8/view/1282996546.43506.jpg"},{"width":"194px","asset_id":"7","style":"view","path":"/asset/image/7/view/1282996485.3278.jpg"},{"width":"194px","asset_id":"9","style":"view","path":"/asset/image/9/view/1282996564.32426.jpg"}]}]'
+      }, {
+        :title => 'Invalid mobile campaign',
+        :document_model => '[{"type":"header","value":""},{"type":"images","value":[]}]'
+      }
+    ].each do |attributes|
+      it "should not create a new instance for: #{attributes[:title]}" do
+        lambda { MobileCampaign.create!(attributes) }.should raise_error
+      end
+    end
+  end
+  
+end
