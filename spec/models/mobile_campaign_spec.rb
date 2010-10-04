be513b654a0a9764c88dbd5446f1146249029632
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2010-10-04 20:06:54 +0300

    Annotated models

--- spec/models/mobile_campaign_spec.rb
+++ spec/models/mobile_campaign_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Info
+#
+# Table name: mobile_campaigns
+#
+#  id             :integer(4)      not null, primary key
+#  document_model :text
+#  style_model    :text
+#  title          :string(255)
+#  created_at     :datetime
+#  updated_at     :datetime
+
 require 'spec_helper'
 
 describe MobileCampaign do
