2753c990f7d72616aed424fec5aff6c2c4545488
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2010-10-04 20:06:00 +0300

    Added geoip_city gem

--- lib/ip_location.rb
+++ lib/ip_location.rb
@@ -1,44 +1,22 @@
-require 'net/geoip'
+require 'geoip_city'
 
 module IpLocation
   
   class << self
   
     def find(ip)
-      @geoip ||= Net::GeoIP.new 'db/max_mind/GeoLiteCity.dat'
-      response = {}
-      begin
-        (response = @geoip[ip]) and (response = make_hashed(response))
-      rescue Net::GeoIP::RecordNotFoundError=> e
-        # make exception notification
-        RAILS_DEFAULT_LOGGER.warn(e)
-      end
-    
-      response
+      @geo_db ||= GeoIPCity::Database.new('db/max_mind/GeoLiteCity.dat', :memory, true)
+      response = @geo_db.look_up(ip)
     end
   
     def resolve_location_for(click)
-      location = find(click.ip_address)
-
-      unless location.empty?
-        click.country = Country.find_or_create_by_name_and_code(location[:country], location[:country_code])
+      if location = find(click.ip_address)
+        click.country = Country.find_or_create_by_name_and_code(location[:country_name], location[:country_code3])
         click.region  = Region.find_or_create_by_code_and_country_id(location[:region], click.country.id) if click.country
         click.city    = City.find_or_create_by_name_and_country_id_and_region_id(location[:city], click.country.id, click.region.id) if click.country and click.region
-        click.located = true
+        (click.country && click.region && click.city) ? click.located = true : RAILS_DEFAULT_LOGGER.warn("Can't lookup ip: " + click.ip_address)
       end
-
     end
-  
-    private
-  
-      def make_hashed(response)
-        hash = {
-          :city=> response.city,
-          :region=> response.region,
-          :country=> response.country_name,
-          :country_code=> response.country_code3
-        }
-      end
-    
+
    end # end class << self
 end
--- spec/lib/ip_location_spec.rb
+++ spec/lib/ip_location_spec.rb
@@ -3,12 +3,13 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe IpLocation do
   it 'should recognize some predefined ip' do
     result = IpLocation.find('77.88.216.22')
-    result[:country_code].should eql('UKR')
-    result[:country].should      eql('Ukraine')
-    result[:region].should       eql('13') 
-    result[:city].should         eql('Kiev')
+    result[:country_code3].should eql('UKR')
+    result[:country_name].should       eql('Ukraine')
+    result[:region].should        eql('13') 
+    result[:city].should          eql('Kiev')
     
-    IpLocation.find('127.0.0.1').to_json.should eql("{}")
+    IpLocation.find('127.0.0.1').should be_nil
+    IpLocation.find('127.0.0.1').to_json.should eql("null")
   end
   
   it 'should recognize location from click' do
