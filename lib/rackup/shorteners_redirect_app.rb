3b738dea613b5e767f4c7092847176cee3ac0ece
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2010-10-27 10:44:36 +0300

    ShortenerApp looks in HTTP_X_REAL_IP or REMOTE_ADDR header

--- lib/rackup/shorteners_redirect_app.rb
+++ lib/rackup/shorteners_redirect_app.rb
@@ -9,7 +9,7 @@ module Rackup
         click = build_clicks_params(env) and click = update_location_for(Click.new(click))
         @short_url.clicks << click
 
-        set_headers :status => 301, :response_body => "Ваш запрос будет перенаправлен", :location => @short_url.origin
+        set_headers :status => 301, :response_body => 'Ваш запрос будет перенаправлен', :location => @short_url.origin
       else
         set_headers :status => 404, :response_body => 'Запрашиваем ресурс не найден'
       end
@@ -24,7 +24,7 @@ module Rackup
 
     def build_clicks_params(env)
       click = {}
-      click[:ip_address] = env['REMOTE_ADDR']
+      click[:ip_address] = env['HTTP_X_REAL_IP'] || env['REMOTE_ADDR']
       click[:referer]    = env['HTTP_REFERER']
       click[:user_agent] = UserAgent.find_or_create_by_details(env['HTTP_USER_AGENT']) unless env['HTTP_USER_AGENT'].blank?
       click
