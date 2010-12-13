894f2f0d657d1b6b01c9159478640cb0f33cb493
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2010-12-13 12:55:20 +0200

    Fix migration

--- db/migrate/20101207172929_create_users.rb
+++ db/migrate/20101207172929_create_users.rb
@@ -3,21 +3,21 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string    :full_name
       t.string    :email,               :null => false
-      t.string    :crypted_password,    :null => false 
-      t.string    :password_salt,       :null => false 
-      t.string    :persistence_token,   :null => false 
-      t.string    :single_access_token, :null => false 
-      t.string    :perishable_token,    :null => false 
+      t.string    :crypted_password,    :null => false
+      t.string    :password_salt,       :null => false
+      t.string    :persistence_token,   :null => false
+      t.string    :single_access_token, :null => false
+      t.string    :perishable_token,    :null => false
 
-      t.integer   :login_count,         :null => false, :default => 0 
-      t.integer   :failed_login_count,  :null => false, :default => 0 
-      t.datetime  :last_request_at                                    
-      t.datetime  :current_login_at                                   
-      t.datetime  :last_login_at                                      
-      t.string    :current_login_ip                                   
+      t.integer   :login_count,         :null => false, :default => 0
+      t.integer   :failed_login_count,  :null => false, :default => 0
+      t.datetime  :last_request_at
+      t.datetime  :current_login_at
+      t.datetime  :last_login_at
+      t.string    :current_login_ip
       t.string    :last_login_ip
       
-      t.string    :state                                      
+      t.string    :state
 
       t.integer   :account_id
       t.timestamps
--- db/migrate/20101208185144_change_accounts_type_fields.rb
+++ db/migrate/20101208185144_change_accounts_type_fields.rb
@@ -1,8 +1,17 @@
 class ChangeAccountsTypeFields < ActiveRecord::Migration
+
   def self.up
     rename_column :accounts, :type, :kind_of
+    
+    account = Account.create :title => 'Yamco', :kind_of => 'yamco', :state => 'activated'
+    user    = User.create :email => 'smbs.app@gmail.com', :full_name => 'SMBS app user', :password => 'smbskickass!', :password_confirmation => 'smbskickass!', :state => 'activated'
+    
+    account.users << user
   end
 
   def self.down
+    Account.find_by_title('Yamco').destroy
+    User.find_by_email('smbs.app@gmail.com').destroy
   end
+
 end
