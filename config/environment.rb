d5ad5a0fb2b3c02a7f56238fb2b0f97746a6d255
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2011-01-05 13:36:28 +0200

    Fix :en locale in production mode

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Smbs::Application.initialize!
+
+# Fix issue when I18n.locale is :en ONLY in production mode
+I18n.locale = :ru
+
