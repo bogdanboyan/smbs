772ec7b65ca69243fbe0bd60bc51813dc40a7bca
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2011-01-21 22:58:43 +0200

    finalize notifications texts

--- app/mailers/mobile_campaign_mailer.rb
+++ app/mailers/mobile_campaign_mailer.rb
@@ -5,7 +5,7 @@ class MobileCampaignMailer < ActionMailer::Base
   
   
   def publish_notice(mobile_campaign)
-    @user, @title  = fetch_info(mobile_campaign)
+    @user, @title       = fetch_info(mobile_campaign)
     
     @settings_url       = settings_mobile_campaign_url(mobile_campaign)
     @mobile_preview_url = mobile_app_campaign_url(mobile_campaign)
@@ -15,15 +15,17 @@ class MobileCampaignMailer < ActionMailer::Base
   end
   
   def cancel_response_notice(mobile_campaign)
-    @user, @title = fetch_info(mobile_campaign)
+    @user, @title  = fetch_info(mobile_campaign)
     
-    @edit_url           = edit_mobile_campaign_url(mobile_campaign)
+    @edit_url      = edit_mobile_campaign_url(mobile_campaign)
     
     mail :to => @user.email, :subject => "Мобильной странице '#{@title}' было Отказано в публикации"
   end
   
   def unpublish_notice(mobile_campaign)
-    @user, @title = fetch_info(mobile_campaign)
+    @user, @title  = fetch_info(mobile_campaign)
+    
+    @shortener_url = mobile_campaign.short_url.link
     
     mail :to => @user.email, :subject => "Мобильная страница '#{@title}' была Снята с Публикована"
   end
