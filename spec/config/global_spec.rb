111fb7100771d26f09c2c24f48ba28d7bc0feb1d
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2011-02-16 12:13:36 +0200

    code clean up

--- spec/config/global_spec.rb
+++ spec/config/global_spec.rb
@@ -9,12 +9,6 @@ describe Global do
     its('host')      { should == 'yamco.local'      }
     its('host_mobi') { should == 'yamco.mobi.local' }
     
-    # before(:all) { @global = Global.configuration }
-    # 
-    # specify 'hosts' do
-    #   @global.host.should      == 'yamco.local'
-    #   @global.host_mobi.should == 'yamco.mobi.local'
-    # end
   end
   
   describe 'production env' do
@@ -23,17 +17,6 @@ describe Global do
     
     its('host')      { should == 'yam.co.ua'  }
     its('host_mobi') { should == 'yamco.mobi' }
-    
-    
-    # before(:all) do 
-    #   Global.reload!('production')
-    #   @global = Global.configuration
-    # end
-    # 
-    # specify 'hosts' do
-    #   @global.host.should      == 'yam.co.ua'
-    #   @global.host_mobi.should == 'yamco.mobi'
-    # end
     
   end
 end
