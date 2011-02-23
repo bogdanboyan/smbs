c6598b9656d602f9a3afcf4dd0c98afad0da6c38
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2011-02-23 16:51:47 +0200

    added textarea resize button to text_partial

--- public/javascripts/mobcn/builder/texts_behaviour.js
+++ public/javascripts/mobcn/builder/texts_behaviour.js
@@ -4,14 +4,37 @@ var TextsBehaviour = PartialBehaviour.extend({
   /*public*/
   apply : function(/*JQuery*/ element, /*Object*/ data) { this._super(element);
     text_container = element.find('.text_container');
-
-    if(data) { text_container.find('textarea').val(data.value); }
+    textarea       = text_container.find('textarea');
+    
+    if(data) { 
+      textarea.val(data.value); 
+    }
+    
+    //textarea resizer
+    element_resizer = element.find('.element_resizer');
+    
+    element_resizer.mousedown(function(e) {
+      start_y = e.pageY
+      element.bind('mousemove', function(e) {
+        absolute_y = e.pageY - start_y
+        textarea_height = parseInt(textarea.css('height')) + absolute_y
+        if(textarea_height >= 25 && textarea_height <= 800) {
+          start_y += absolute_y
+          textarea.css('height', textarea_height)
+        }
+      })
+    })
+    
+    jQuery('body').mouseup(function(e) {
+      element.unbind('mousemove')
+    })
+    
     element.show();
   },
   
   
   to_object : function(/*JQuery*/ element) {
-    this.partial_model.value = $(element).find('textarea').val();
+    this.partial_model.value = jQuery(element).find('textarea').val();
     return this.partial_model;
   },
 
