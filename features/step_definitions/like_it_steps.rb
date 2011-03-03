3cfd0aec702bbe627e7a3498f06d95d94cb3f855
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2011-03-03 21:39:02 +0200

    fixed broken cukes

--- features/step_definitions/like_it_steps.rb
+++ features/step_definitions/like_it_steps.rb
@@ -1,12 +1,10 @@
 # encoding: utf-8
 
-Допустим /^существует LikeIt кнопка:$/ do |таблица|
+Допустим /^существует LikeIt кнопка:$/ do |table|
   @like_its = []
-  таблица.hashes.each do |row|
-    @like_its << LikeIt.create(row)
-  end
   
-  MobileCampaign.first.like_its = @like_its
+  table.hashes.each { |row| @like_its << LikeIt.create(row) }
+  
   @like_it = @like_its.first
 end
 
@@ -16,12 +14,11 @@ end
   like_it.clicks_count.should == 1
   like_it.clicks.size.should  == 1
   like_it.clicks.first.ip_address.should == '127.0.0.1'
-  
 end
 
 Тогда /^система не должна регистрировать этот запрос LikeIt кнопки "([^\"]*)"$/ do |tag|
   like_it = LikeIt.find_by_tag tag
   
-  like_it.clicks_count.should == 0
+  like_it.clicks_count.should be_nil
   like_it.clicks.size.should  == 0
 end
--- spec/factories/mobile_campaigns.rb
+++ spec/factories/mobile_campaigns.rb
@@ -3,7 +3,7 @@ Factory.define :mobile_campaign do |t|
   t.id             1
   t.asset_files    { |i| [ i.association(:image_asset) ] }
   t.title          "Новый браузер Chrome"
-  t.document_model "[{\"type\":\"header\",\"value\":\"h3. Новый быстрый браузер: теперь и для Mac!\"},{\"type\":\"text\",\"value\":\"\\\"Google Chrome\\\":http://www.google.com/chrome?hl=ru загружает веб-страницы и приложения с молниеносной скоростью.\"},{\"type\":\"images\",\"value\":[{\"width\":\"196px\",\"asset_id\":\"100\",\"style\":\"view\",\"path\":\"/assets/mobcn/100/1285524080.view.jpg?1285524080\"}]}]"
+  t.document_model "[{\"type\":\"header\",\"value\":\"h3. Новый быстрый браузер: теперь и для Mac!\"},{\"type\":\"text\",\"value\":\"\\\"Google Chrome\\\":http://www.google.com/chrome?hl=ru загружает веб-страницы и приложения с молниеносной скоростью.\"},{\"type\":\"images\",\"value\":[{\"width\":\"196px\",\"asset_id\":\"100\",\"style\":\"view\",\"path\":\"/assets/mobcn/100/1285524080.view.jpg?1285524080\"}]}, {\"type\":\"likeit\",\"title\":\"Мне нравится!\",\"tag\":\"@tag\",\"position\":\"posR\"}]"
 end
 
 Factory.define :mbc_with_html_code, :class => MobileCampaign do |t|
