8584a719a2d564f5a1f4b41226b91459b528ff5f
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2011-03-03 20:13:49 +0200

    redirect_to return_to url when user is logged out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
   def create
     @user_session = UserSession.new params[:user_session]
     @current_user = @user_session.user if @user_session.save
-    @current_user ? redirect_to(account_dashboard_url) :  render(:action => :new)
+    @current_user ? redirect_to(cookies[:return_to] || account_dashboard_url) :  render(:action => :new)
   end
 
   def destroy
