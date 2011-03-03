e45d7510e5e52104c6bc91bdd2c95353d0285217
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2011-03-03 15:10:57 +0200

    added likeit interaction

--- app/controllers/mobile_app/campaigns_controller.rb
+++ app/controllers/mobile_app/campaigns_controller.rb
@@ -9,7 +9,13 @@ class MobileApp::CampaignsController < MobileApp::BaseController
     # we want receive email notification about requests for unknown campaigns ids
     if MobileCampaign.exists? params[:id]
       @mbc = MobileCampaign.find params[:id]
-      @document_model = @mbc.document_model_as(:array).map! {|entity| entity.symbolize_keys }
+      
+      @document_model = @mbc.document_model_as(:array).map! do |entity| 
+        entity.symbolize_keys!
+        def entity.is_a_partial?(type); self[:type] == type; end
+        entity
+      end
+      
       load_see_more(@mbc)
     else
       redirect_to_not_found_page
