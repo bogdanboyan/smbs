7c537aab83da87d9b602b0bf8df078421f33f0c9
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2011-03-16 17:52:02 +0200

    added new text and invite flow to welcome page

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -5,13 +5,13 @@ class InviteController < ApplicationController
   
   def create
     invite = Invite.create(params[:invite])
-    if invite.save
-      flash[:notice] = "Ваша заявка принята :) Мы обязательно с вами свяжемся"
+    if is_saved = invite.save
+      message = "Ваша заявка принята. Мы обязательно с вами свяжемся."
     else
-      flash[:error] = "#{invite.errors.full_messages.join(', ')}. Пожайлуста заполните все поля правильно!"
+      message = "Пожалуйста заполните все поля правильно!"
     end
     
-    redirect_to root_url
+    render :json => { :message => message, :error => !is_saved }
   end
   
 end
--- app/models/invite.rb
+++ app/models/invite.rb
@@ -2,7 +2,7 @@ class Invite < ActiveRecord::Base
   
   validates_uniqueness_of :email, :on => :create
   
-  validates_length_of :company, :minimum => 4, :maximum => 25
+  validates_length_of :company, :minimum => 3, :maximum => 25
   validates_length_of :phone, :minimum => 3, :maximum => 13
   
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
