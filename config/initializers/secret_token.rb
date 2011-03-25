4d5e873ded42f3f7e794356fbe640af7e596636e
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2011-03-25 20:32:03 +0200

    changed rails secret

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Smbs::Application.config.secret_token = '0dbf3a21f1fffd5826bc5119c3f518625806385f7ee122cc757f0fdc868d4a74e12c742d07080596a946d6f8bbaaeed0751fac887ca388a2ec56beccc0dbdd9e'
+Smbs::Application.config.secret_token = 'a5db9537df0d7175c70dc06497665a41f94a87d7ca1bff1bab71ee5017acd7fdbf026f3b48062c190adcf65831c6c3403d20ac5aee9e3582d861e8cb583236a0'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Smbs::Application.config.session_store :cookie_store, :key => '_smbs_session'
+Smbs::Application.config.session_store :cookie_store, :key => '_y_s'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
