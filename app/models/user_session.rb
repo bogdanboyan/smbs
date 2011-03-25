e8479b202eb62016bdfa3cef6140c6cfc47f7f1a
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2011-03-25 20:43:13 +0200

    changed properties for AUHLOGIC

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,7 +1,11 @@
 # encoding: utf-8
 class UserSession < Authlogic::Session::Base
+  
+  # Set uuser credential key
+  cookie_key "_y_u"
+  
   # Set expiration for cookie authentication
-  remember_me_for 3.months
+  remember_me_for 1.week
 
   # Enable secure (login|email)/password validation
   generalize_credentials_error_messages "Такой почтовый адрес не зарегистрирован, либо пароль неверный"
