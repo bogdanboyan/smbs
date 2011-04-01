5a45852940783e612047dfad4d87337a25119065
Aleksey Zatvornitskiy <a.zatvornitskiy@gmail.com>
2011-04-01 20:57:51 +0300

    added cukes for mobile campaign creation cases

--- public/javascripts/mobcn/builder.js
+++ public/javascripts/mobcn/builder.js
@@ -9,11 +9,10 @@ var Builder = Class.extend({
     this.document_partials = document_partials;
 
     /* 'this' context as proxied object */
-    var place_container_to_document_proxy = jQuery.proxy(this._place_container_to_document, this);
+    var place_container_to_document_proxy = jQuery.proxy(this.append_document_partial, this);
     
-    /* init document partials listeners */
-    jQuery('#document_partials ul').find('li').each(function(/*<li>*/index) {
-      jQuery(this).click(function() { place_container_to_document_proxy(jQuery(this).attr('id')); });
+    jQuery('#document_partials').delegate('a', 'click', function() {
+      place_container_to_document_proxy(jQuery(this).attr('class'));
     });
 
     /* common behaviour */
@@ -42,23 +41,25 @@ var Builder = Class.extend({
   
   inject_serialized_document : function(model) {
     for(partial in model) {
-      this._place_container_to_document(model[partial].type, model[partial]);
+      this.append_document_partial(model[partial].type, model[partial]);
+    }
+  },
+  
+  append_document_partial : function(id, data) {
+    container_id = PartialRegistry.to_s(id);
+    behaviour = PartialRegistry.init_behaviour(id);
+    
+    if(container_id && behaviour) {
+      partial = jQuery(this.document_partials[container_id]).attr('id', ++this.document_sequence);
+      jQuery(this.document).append(partial);
+      partial.data('behaviour', behaviour);
+      behaviour.apply(partial, data);
     }
   },
 
   /*private*/
-    _place_container_to_document : function(id, data) {
-      container_id = PartialRegistry.to_s(id);
-      behaviour = PartialRegistry.init_behaviour(id);
-      if(container_id && behaviour) {
-        partial = jQuery(this.document_partials[container_id]).attr('id', ++this.document_sequence);
-        jQuery(this.document).append(partial);
-        partial.data('behaviour', behaviour);
-        behaviour.apply(partial, data);
-      }
-    },
     
-    _all_document_partials : function() {
-      return jQuery(this.document).find(this.document_partial);
-    }
+  _all_document_partials : function() {
+    return jQuery(this.document).find(this.document_partial);
+  }
 });
